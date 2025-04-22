77adab83d8ff03d5cd197f38808c1faa905fa7c4
Чеботин Александр <Chebotin.A@sbertech.ru>
2025-04-23 01:06:44 +0300

    commit

--- storage-core/src/main/java/ru/axothy/storage/SSTableManager.java
+++ storage-core/src/main/java/ru/axothy/storage/SSTableManager.java
@@ -153,29 +153,6 @@ public class SSTableManager {
         return new SSTableIterator(sstable, keyIndexFrom, keyIndexTo, keyOffset);
     }
 
-    /**
-     * Writes SSTable in next format:
-     * SSTable header contains:
-     * ┌─────────────────────┬─────────────────────────┬─────────────────────┐
-     * │          8          │            8            │          8          │
-     * │─────────────────────│─────────────────────────│─────────────────────│
-     * │   BF array length   │  Hash functions count   │    Entries count    │
-     * └─────────────────────┴─────────────────────────┴─────────────────────┘
-     * SStable bloom filter:
-     * ┌────────────────────────────┐
-     * │ 8 x BloomFilter array size │
-     * │────────────────────────────│
-     * │           Hash_i           │
-     * └────────────────────────────┘
-     * where i = 1, ... , bloom filter array size
-     * SStable data format:
-     * ┌─────────────────────┬──────────┬──────────┬────────────┬────────────┐
-     * │  8 x Entries count  │    8     │ Key size │     8      │ Value size │
-     * │─────────────────────│──────────│──────────│────────────│────────────│
-     * │     Key_j offset    │ Key size │    Key   │ Value size │ Value      │
-     * └─────────────────────┴──────────┴──────────┴────────────┴────────────┘
-     * where j = 1, ... , entries count.
-     */
     public MemorySegment write(Collection<Entry<MemorySegment>> dataToFlush, double bloomFilterFPP) throws IOException {
         long size = 0;
 
